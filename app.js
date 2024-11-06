d8e9cf0f2c2503817fb3dbdbdc8c76122b25df97
amadousadialloh <amadousadialloh@gmail.com>
2024-11-06 14:45:24 -0600

    make the calculator works for 1 digit operations

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
-const arrayClicks = [];
-/*-------------------------------- Variables --------------------------------*/
 
+/*-------------------------------- Variables --------------------------------*/
+let result = null;
+let firstNumber = null;
+let secondNumber = null;
+let isSeconNumberClicked = false;
+let operatorContent = null;
+let numberButtonContent = null;
+let displayCleard = false;
 /*------------------------ Cached Element References ------------------------*/
-const buttonElements = document.querySelectorAll(".row button");
+const numbersButtonElements = document.querySelectorAll(".number");
+const operatorElement = document.querySelectorAll(".operator");
 const displayElements = document.querySelector(".display");
-const showResult = document.querySelector(".button equals");
+const equalButton = document.querySelector(".equals");
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -31,34 +38,68 @@ const divideNumbers = (number1, number2) => {
   return number1 / number2;
 };
 
-//function to handle all the clicks events for operands
-const handleClickOperandButtons = () => {
-  buttonElements.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const number = Number(event.target.textContent);
-      if (!isNaN(number)) {
-        arrayClicks.push(number);
-      } else {
-        // Check if the button clicked is an operator
-        if (event.target.textContent === "+") {
-          operations(addNumbers);
-        } else if (event.target.textContent === "-") {
-          operations(subtractNumbers);
-        } else if (event.target.textContent === "*") {
-          operations(multiplyNumbers);
-        } else if (event.target.textContent === "/") {
-          operations(divideNumbers);
-        }
-      }
-    });
-  });
-};
-
-handleClickOperandButtons();
-
-const operations = (functionOperation) => {
-  let result = arrayClicks[0];
-  for (let i = 0; i < arrayNumbers.length; i++) {
-    result = functionOperation(result, arrayClicks[i]);
+//operation
+const operation = (firstNumber, operator, secondNumber) => {
+  if (operator === "+") {
+    return addNumbers(firstNumber, secondNumber);
+  }
+  if (operator === "-") {
+    return subtractNumbers(firstNumber, secondNumber);
+  }
+  if (operator === "*") {
+    return multiplyNumbers(firstNumber, secondNumber);
+  }
+  if (operator === "/") {
+    return divideNumbers(firstNumber, secondNumber);
   }
 };
+//clear the display after operation
+const clearDisplay = () => {
+  displayElements.textContent = " ";
+  result = null;
+  firstNumber = null;
+  secondNumber = null;
+  isSeconNumberClicked = false;
+  operatorContent = null;
+  numberButtonContent = null;
+};
+
+numbersButtonElements.forEach((numberButton) => {
+  numberButton.addEventListener("click", (e) => {
+    numberButtonContent = Number(numberButton.textContent);
+    if (!isSeconNumberClicked) {
+      firstNumber = numberButtonContent;
+    } else {
+      secondNumber = numberButtonContent;
+    }
+  });
+});
+
+//operator click control
+operatorElement.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    operatorContent = operator.textContent;
+    if (operatorContent === "+") {
+      firstNumber = numberButtonContent;
+      isSeconNumberClicked = true;
+    } else if (operatorContent === "-") {
+      firstNumber = numberButtonContent;
+      isSeconNumberClicked = true;
+    } else if (operatorContent === "*") {
+      firstNumber = numberButtonContent;
+      isSeconNumberClicked = true;
+    } else if (operatorContent === "/") {
+      firstNumber = numberButtonContent;
+      isSeconNumberClicked = true;
+    } else {
+      clearDisplay();
+    }
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  result = operation(firstNumber, operatorContent, secondNumber);
+  displayElements.textContent = result;
+});
+
+clearDisplay();
